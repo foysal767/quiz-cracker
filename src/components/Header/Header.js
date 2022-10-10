4d00ae2cdf276ca8e7354ecdeef223b42ea3d9bf
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-11 00:49:47 +0600

    navbar added with logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,13 +7,14 @@ const Header = () => {
         <div>
             <Navbar bg="primary" variant="dark" expand="lg">
                 <Container className='container'>
-                    <Link className='text-white no-underline' to="/">Navbar</Link>
+                    <img style={{ width: 60, height: 60 }} className='' src="https://www.freeiconspng.com/thumbs/education-png/education-png-24.png" alt="" />
+                    <Link style={{ textDecorationLine: 'none' }} className='text-white ms-2' to="/">TopQuizCoder</Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Link className='text-white ms-2' to="/">Home</Link>
-                        <Link className='text-white ms-2' to="statistics">Statistics</Link>
-                        <Link className='text-white ms-2' to="blogs">Blogs</Link>
+                        <Link style={{ textDecorationLine: 'none' }} className='text-white ms-2' to="/">Home</Link>
+                        <Link style={{ textDecorationLine: 'none' }} className='text-white ms-2' to="statistics">Statistics</Link>
+                        <Link style={{ textDecorationLine: 'none' }} className='text-white ms-2' to="blogs">Blogs</Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
