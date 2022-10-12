96caa627957d4ff3636bf4e15d0be3d57c108711
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-12 17:07:59 +0600

    banner text edited

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,12 +8,14 @@ const Home = () => {
     return (
         <Container className='home'>
             <div>
-                <div className='my-4' style={{ backgroundImage: `url("https://images.unsplash.com/photo-1501139083538-0139583c060f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`, height: "90vh", backgroundSize: "cover", backgroundPosition: "center" }}>
-                    <div className='text-white position-absolute top-50 px-auto'>
-                        <h2 >World's Best Quiz Website</h2>
-                        <p>TopQuizCoder is a web-based Audience Engagement Cloud Platform for test personal skills about coding. It's also help learing.</p>
+                <Row>
+                    <div className='my-4' style={{ backgroundImage: `url("https://images.unsplash.com/photo-1501139083538-0139583c060f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`, height: "90vh", backgroundSize: "cover", backgroundPosition: "center" }}>
+                        <div className='text-start text-white position-absolute top-50'>
+                            <h2 className='ms-4 fs-1'>World's Best Quiz Website</h2>
+                            <p className='ms-4 fs-5'>TopQuizCoder is a web-based Audience Engagement Cloud Platform for <br /> the test personal skills about coding. It's also help learing.</p>
+                        </div>
                     </div>
-                </div>
+                </Row>
                 <Row xs={1} md={3} className="g-12 text-center mx-auto bg-info px-4 py-5 rounded">
                     {
                         (topics.data).map(topic => <Topic
