16c4d6a47e1fef076a84133610f8efefe4cba735
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-12 07:35:29 +0600

    some banner part css work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     return (
         <Container className='home'>
             <div className='my-4' style={{ backgroundImage: `url("https://images.unsplash.com/photo-1501139083538-0139583c060f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`, height: "90vh", backgroundSize: "cover", backgroundPosition: "center"}}>
-                <h2 className='text-white my-auto'>This is header part</h2>
+                <h2 className='text-white'>World's Best Quiz Website</h2>
             </div>
             <Row xs={1} md={3} className="g-12 text-center mx-auto bg-info px-4 py-5 rounded">
             {
